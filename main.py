45b1c4b5534d75b3cf3ef6a9dee7ffbb921217c5
apolloparty <apolloparty@gmail.com>
2019-10-23 01:00:47 +0200

    Main added

---
+++ main.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# -*- coding: Utf-8 -*
+
+import json
+import requests
+import sys
+import os.path
+import mysql.connector
+from random import randint
+from mysql.connector import errorcode
+import Class.Sql as S
+import Class.Searching as Se
+import Class.Calc as C
+import Class.Listing as L
+import Class.User_interface as U
+
+mydb = mysql.connector.connect(
+            host="localhost",
+            user="testeur",
+            passwd="testeur",
+            database="test"
+            )
+mycursor = mydb.cursor()
+#mycursor.execute(f"DROP TABLE IF EXISTS PRODUCTS")
+#mycursor.execute(f"DROP TABLE IF EXISTS SAVED")
+#mycursor.execute(f"DROP TABLE IF EXISTS CATEGORY")
+reboot = U.User_interface(mycursor).introduct()
+category, category_name = U.User_interface(mycursor).chosen_cat()
+name, product_type, category = U.User_interface(mycursor).chosen_prod(category)
+if reboot == "1":
+    product_nb, product_gbl, product_dict = Se.Searching(name).first_search()
+    pages = C.Calc(product_nb, product_gbl).calc_pages()
+    global_dict = Se.Searching(name).global_search(pages)
+    global_dict.update(product_dict)
+    names_list, codes_list, description_list, store_list, product_show = L.Listing(product_nb, pages, name).listing()
+    Sql(names_list, codes_list, description_list, store_list, product_show, name, category_name, category).drop_table(mycursor)
+else :
+    S.Sql(name, category_name, category, mydb, product_type).table_category(mycursor)
+if reboot == "1":
+    S.Sql(name, category_name, category, mydb, product_type).table_product(mycursor, names_list, codes_list, description_list, store_list, product_show)
+else:
+    selected1 = S.Sql(name, category_name, category, mydb, product_type).show_product(mycursor)
+    selected2 = S.Sql(name, category_name, category, mydb, product_type).alt_product(mycursor, selected1)
+    S.Sql(name, category_name, category, mydb, product_type).save_product(mycursor, selected2)
+    S.Sql(name, category_name, category, mydb, product_type).display_saved(mycursor)
